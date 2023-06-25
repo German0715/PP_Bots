4f8f019542d51f3e9ff6ed9b1e5ae214445a1bff
German0715 <121468872+German0715@users.noreply.github.com>
2023-06-25 08:46:51 -0500

    Update BOT_Template.py

--- BOT_Template.py
+++ BOT_Template.py
@@ -1,1 +1,25 @@
+#Template para que usen con la clase BOT! 
+
+#Importando librerías (se modifica al gusto)
+
+import discord
+import requests  #Asegúrese de que tiene instalada la biblioteca requests. Si no es así, ¡instálala con pip install!
+from discord.ext import commands
+import os
+import random
+
+#NO BORRAR 
+# La variable intents almacena los privilegios del bot
+intents = discord.Intents.default()
+
+# Activar el privilegio de lectura de mensajes
+intents.message_content = True
+
+# Crear un bot en la variable cliente y transferirle los privilegios
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+#Para saber si hemos iniciado sesión
+@bot.event
+async def on_ready():
+    print(f"Hemos iniciado sesión como {bot.user}")
 
