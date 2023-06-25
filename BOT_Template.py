9b0d974e3ce1013c5c5272f7f9657f4dd52d9cca
German0715 <121468872+German0715@users.noreply.github.com>
2023-06-25 08:52:39 -0500

    Update BOT_Template.py

--- BOT_Template.py
+++ BOT_Template.py
@@ -15,11 +15,20 @@ intents = discord.Intents.default()
 # Activar el privilegio de lectura de mensajes
 intents.message_content = True
 
-# Crear un bot en la variable cliente y transferirle los privilegios
-bot = commands.Bot(command_prefix='!', intents=intents)
+# Crear un bot en la variable cliente y transferirle los privilegios, también se define que el bot funcione con ! 
+bot = commands.Bot(command_prefix='!', intents=intents) 
 
 #Para saber si hemos iniciado sesión
 @bot.event
 async def on_ready():
     print(f"Hemos iniciado sesión como {bot.user}")
 
+
+#Así se hacen los comandos para que el bot opere
+@bot.command()  
+async def Micomando(ctx):   #De aquí depende el comando, el nombre que se pone aquí es el comando que el bot usará (!Micomando)
+    #AQUÍ VA EL FUNCIONAMIENTO DEL BOT, dependiendo de lo que quieran que haga    
+    await ctx.send(loqueenvio)  #acá es lo que el BOT va a responderte cuando escribas !Micomando
+
+#Para correr el BOT
+bot.run("YOUR TOKEN HERE") # TOKEN --> No borrar 
